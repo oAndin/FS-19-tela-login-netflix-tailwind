45547ed4e2f77317e6f73e7a9f64602e50e91168
oAndin <00andherson@gmail.com>
2023-06-11 19:47:04 -0300

    Bom, acho que posse ter feito besteira

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      spacing: {
+        quadrado:'450px',
+        retangulo:'550px'
+      }
+    },
+  },
+  plugins: [],
+}
