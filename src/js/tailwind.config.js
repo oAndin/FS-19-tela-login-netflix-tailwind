7575e08243b52b56da02e3708d7e8e7a6c64d54d
oAndin <00andherson@gmail.com>
2023-06-11 08:10:47 -0300

    Consegui

---
+++ src/js/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            spacing: {
+                quadrado: '450px',
+                retangulo: '550px'
+            }
+        }
+    }
+}
